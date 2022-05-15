902989f1d1e46f3c1e40bd5ec0eae1016d5e488e
charliegallop <c.gallop@protonmail.com>
2022-05-15 12:35:43 +0100

    beta version

---
+++ code/functions.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import math
+from scipy.stats import variation
+from pylab import rcParams
+import statsmodels.api as sm
+import os
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_squared_error
+warnings.filterwarnings('ignore')
+sns.set_style('darkgrid')
+
+
+def read_data(file_directory = None, resample_period = 'D', resample_aggregate = 'mean'):
+    '''
+    file_directory: str, directory of csv file to be read
+    resample_period: str, 'H' for hours, 'D' for days
+    resample_aggregate: list, will create a new column for each aggregate
+    '''
+    
+    df = pd.read_csv(file_directory)
+    df['ds'] = pd.to_datetime(df['ds'])
+    df = df.set_index('ds')
+
+    if resample_aggregate == 'mean':
+        df = df.resample(resample_period).mean()
+        df = df.add_suffix(f"_{resample_aggregate}")
+
+    if resample_aggregate == 'max':
+        df = df.resample(resample_period).max()
+        df = df.add_suffix(f"_{resample_aggregate}")
+        
+        
+    if resample_aggregate == 'min':
+        df = df.resample(resample_period).min()
+        df = df.add_suffix(f"_{resample_aggregate}")
+        
+    if resample_aggregate == 'sum':
+        df = df.resample(resample_period).sum()
+        df = df.add_suffix(f"_{resample_aggregate}")
+        
+    df = df.fillna(0)
+
+    return df
+
+def add_aggregate(df = None, resample_aggregate = None, resample_period = None):
+
+    if resample_aggregate == 'mean':
+        df2 = df.resample(resample_period).mean()
+
+    if resample_aggregate == 'max':
+        df2 = df.resample(resample_period).max()
+
+    if resample_aggregate == 'min':
+        df2 = df.resample(resample_period).min()
+
+    if resample_aggregate == 'sum':
+        df2 = df.resample(resample_period).sum()
+
+    df = pd.merge(df, df2, left_index = True, right_index = True, suffixes = ("", f"_{resample_aggregate}"))
+
+    return df
+
+
+
+    # df_dict = {}
+
+    # if 'mean' in resample_aggregates:
+
+    #     df_mean = df.resample(resample_period).mean()
+    #     df_dict['mean'] = ['mean', df_mean]
+
+    # if 'sum' in  resample_aggregates:
+
+    #     df_sum = df.resample(resample_period).sum()
+    #     df_dict['sum'] = ['sum', df_sum]
+
+    # if 'max' in resample_aggregates:
+
+    #     df_max = df.resample(resample_period).max()
+    #     df_dict['max'] = ['max', df_max]
+
+    # if 'min' in resample_aggregates:
+
+    #     df_min = df.resample(resample_period).min()
+    #     df_dict['min'] = ['min', df_min]
+
+    
+    # reduce(lambda: df_dict.keys())
+    
+    
+    # if len(df_dict) > 1:
+    #     for i in df_dict:
+    #         df = pd.merge(df, df_dict[i], 
+    #         left_index = True, 
+    #         right_index = True,
+    #         suffixes = (f"_{i}", f"_{i}"))
+
+
+
+
+    # return df
--- code/wranglingData.py
+++ code/wranglingData.py
@@ -4,36 +4,96 @@ Created on Wed Jan 19 15:03:24 2022
 
 @author: cg639
 """
+
+# %%
 Dir_rawData = "C:/Users/cg639/University of Exeter/MTHM604-Tackling Sustainability Challenges using Data and Models-TERM2 (2021-22) - Group 2 - Group 2/MTHM604_week_1/data/rawData"
 Dir_indoorAq = "C:/Users/cg639/University of Exeter/MTHM604-Tackling Sustainability Challenges using Data and Models-TERM2 (2021-22) - Group 2 - Group 2/MTHM604_week_1/data/rawData/ParticulateMatter"
 Dir_cleanData = "C:/Users/cg639/University of Exeter/MTHM604-Tackling Sustainability Challenges using Data and Models-TERM2 (2021-22) - Group 2 - Group 2/MTHM604_week_1/data/cleanData"
 Dir_externalAq = "C:/Users/cg639/University of Exeter/MTHM604-Tackling Sustainability Challenges using Data and Models-TERM2 (2021-22) - Group 2 - Group 2/MTHM604_week_1/data/rawData/External"
-
+Dir_indoorVOC = "C:/Users/cg639/University of Exeter/MTHM604-Tackling Sustainability Challenges using Data and Models-TERM2 (2021-22) - Group 2 - Group 2/MTHM604_week_1/data/rawData/VolatileOrganicCompounds"
 
 import os
 import pandas as pd
+from tqdm import tqdm
 
-def CleanFiles(directory, typeAq):
+# %%
+def CleanFiles(directory, typeAq, sType):
+    count = 0
     for filename in os.listdir(directory):
         splitName = filename.split(sep = '_')
         uprns = splitName[1]
         sid = splitName[2][3:10]
-        file = pd.read_csv(f"{directory}/{filename}")
-        file['sid'] = sid
-        file['sid'] = file['sid'].astype(str)
-        file['uprns'] = uprns
-        file['uprns'] = file['uprns'].astype(str)
-        file.to_csv(f"{Dir_cleanData}/{typeAq}/{filename}")
+        df = pd.read_csv(f"{directory}/{filename}")
+        
+        if typeAq == "indoorAq":
+            df.columns.values[1] = "PM2_5"
+        elif typeAq == "externalAq":
+            df.columns.values[1] = "humidity"
+            df.columns.values[2] = "temperature"
+            df.columns.values[3] = "VOC"
+            df.columns.values[4] = "PM2_5"
+            df.columns.values[5] = "PM10"
+        elif typeAq == "indoorVOC":
+            df.columns.values[1] = "VOC"
+            
+        df['sid'] = sid
+        df['sid'] = df['sid'].astype(str)
+        df['uprns'] = uprns
+        df['uprns'] = df['uprns'].astype(str)
+        df['sensType'] = sType
+        df['sensType'].astype(str)
+        df.to_csv(f"{Dir_cleanData}/{typeAq}/{filename}")
         print(f"Finished {filename}")
-    print("Finished all files!")
+        count += 1
     
-#CleanFiles(Dir_indoorAq, "indoorAq")
-CleanFiles(Dir_externalAq, "externalAq")
+    print("Finished all files!")
+    print(f"Number of files processed {count}/{len(os.listdir(directory))}")
+
+# %%
+
+def ConcatAllFiles(directory):
+    failedFiles = []
+    df=pd.DataFrame()
+    totalCount = 0
+    totalNum = 0
+    for folder in os.listdir(directory):
+        if totalCount == 0:
+            print(os.listdir(directory))
+        count = 0
+        folderDir = os.listdir(f"{directory}/{folder}")
+        for filename in folderDir:
+            try:
+                aux=pd.read_csv(f"{directory}/{folder}/{filename}")
+                df=pd.concat([df, aux], sort = False)
+                totalCount += 1
+                count += 1
+            except:
+                print(f"{filename} failed to concatenate")
+                failedFiles.append(filename)
+
+            totalNum += 1
+        print(f"Succesfully concatenated {count}/{len(folderDir)} files from {folder}")
+    print(f"Concatenate Done! {totalCount}/{totalNum}")
+    print(f"Failed files: {failedFiles}")
+    return df, failedFiles
+
+#df,failedFiles = ConcatAllFiles("C:/Users/cg639/University of Exeter/MTHM604-Tackling Sustainability Challenges using Data and Models-TERM2 (2021-22) - Group 2 - Group 2/MTHM604_week_1/data/cleanData")
+
+test, ff = ConcatAllFiles("C:/Users/cg639/Downloads/test")
+
+# %%
+  
+CleanFiles(Dir_indoorAq, "indoorAq", "IN")
+CleanFiles(Dir_externalAq, "externalAq", "EX")
+CleanFiles(Dir_indoorVOC, "indoorVOC", "IN")
+
 
 #%%
-df = pd.read_csv("C:/Users/cg639/University of Exeter/MTHM604-Tackling Sustainability Challenges using Data and Models-TERM2 (2021-22) - Group 2 - Group 2/MTHM604_week_1/data/rawData/indoorAq/ts_0001_sid6322063.csv")
+df = pd.read_csv("C:/Users/cg639/University of Exeter/MTHM604-Tackling Sustainability Challenges using Data and Models-TERM2 (2021-22) - Group 2 - Group 2/MTHM604_week_1/data/rawData/ParticulateMatter/ts_0001_sid6322063.csv")
 df.head()
     
+#df = pd.read_csv("C:/Users/cg639/University of Exeter/MTHM604-Tackling Sustainability Challenges using Data and Models-TERM2 (2021-22) - Group 2 - Group 2/MTHM604_week_1/data/cleanData/indoorAq/ts_0001_sid6322063.csv")
+#df.head()
     
     
 
